9778caab0f2b3d44ffde7fbe726e81b4abdb0e8e
Student 12 <user12@tc.geeksforless.net>
2017-11-01 19:23:07 +0200

    1class

--- server/api/shop/libs/Controllers/Room.php
+++ server/api/shop/libs/Controllers/Room.php
@@ -9,9 +9,18 @@ class Room
     {
         try
         {
-            $result = \Models\Room::findAll();
-            $result = \Response::typeData($result,$type);
-            return \Response::ServerSuccess(200, $result);
+            if($data[1] === null )
+            {
+                $result = \Models\Room::findAll();
+                $result = \Response::typeData($result,$type);
+                return \Response::ServerSuccess(200, $result);
+            }
+            else
+            {
+                $result = \Models\Room::findByid($data[0]);
+                $result = \Response::typeData($result,$type);
+                return \Response::ServerSuccess(200, $result);
+            }
         }
         catch(\Exception $exception)
         {
